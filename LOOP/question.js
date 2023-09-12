27dc46677a9899f5b1a9dfd7656c3fe56c1ca5df
KopalGangrade <126078281+KopalGangrade@users.noreply.github.com>
2023-09-12 05:44:20 +0000

    add more console

--- LOOP/question.js
+++ LOOP/question.js
@@ -1,1 +1,14 @@
 console.log("Hello World");
+
+var Name="kopal"
+let date=20
+const city="Pune"
+
+date=26
+console.log(Name)
+console.log(date)
+
+// can't change const value
+// var and let value can modify
+
+
