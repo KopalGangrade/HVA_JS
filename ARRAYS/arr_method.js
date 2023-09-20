3f6c7f1a9e667032a8e704d631f6dfed0ffaac0f
KopalGangrade <126078281+KopalGangrade@users.noreply.github.com>
2023-09-20 17:00:29 +0000

    method1

---
+++ ARRAYS/arr_method.js
@@ -0,0 +1,36 @@
+
+// ARRAY METHODS
+
+const myarr= [1,2,3,4,5]
+// console.log(myarr)
+// myarr.push(7)
+// console.log(myarr)
+// myarr.pop()
+// console.log(myarr)
+
+
+// myarr.unshift(7) // number will come at first
+
+
+
+// console.log(myarr)
+
+// console.log(myarr.includes(5));  // checks vakue is included in array or not.
+
+
+// const newarr1=myarr.join();  // convert into string
+// console.log(typeof newarr1);
+// console.log(newarr1)
+
+
+
+//slice , splice
+
+console.log("A ",myarr)
+const my1=myarr.slice(1,3)
+console.log(my1)
+console.log("b ",myarr)
+
+const my2=myarr.splice(1,3)
+console.log("c ",myarr)
+console.log(my2)
